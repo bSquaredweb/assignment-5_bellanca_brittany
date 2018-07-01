7c7e782237c5aaf99f83f8916fb5aee29514c997
Brittany Bellanca <brittanybellanca@gmail.com>
2018-07-01 11:47:36 -0400

    updated counting method

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 var heading = document.getElementById('h1');
-var clickNumber = 1;
 
 heading.addEventListener('click', function(event) {
   var newParagraph = document.createElement('p');
-  newParagraph.innerHTML = 'This is click number' + ' ' + clickNumber++;
+  newParagraph.innerHTML = 'This is click number' + ' ' + event.detail;
   document.getElementsByTagName('body')[0].appendChild(newParagraph);
 });
