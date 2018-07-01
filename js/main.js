76bfe1ac95d5cb2c3ef98416cc3303ef0e9bb80d
Brittany Bellanca <brittanybellanca@gmail.com>
2018-07-01 11:41:33 -0400

    new paragraph now appears with each click per directions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var heading = document.getElementById('h1');
-var newParagraph = document.createElement('p');
 var clickNumber = 1;
 
-heading.addEventListener('click', function() {
+heading.addEventListener('click', function(event) {
+  var newParagraph = document.createElement('p');
   newParagraph.innerHTML = 'This is click number' + ' ' + clickNumber++;
   document.getElementsByTagName('body')[0].appendChild(newParagraph);
 });
