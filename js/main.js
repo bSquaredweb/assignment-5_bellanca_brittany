cbad2d5b51c5f872562953884bce21d77307ee70
Brittany Bellanca <brittanybellanca@gmail.com>
2018-07-01 16:35:33 -0400

    changed back to increments per professor's email

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 var heading = document.getElementById('h1');
+var clickNumber = 1;
 
-heading.addEventListener('click', function(event) {
+heading.addEventListener('click', function() {
   var newParagraph = document.createElement('p');
-  newParagraph.innerHTML = 'This is click number' + ' ' + event.detail;
+  newParagraph.innerHTML = 'This is click number' + ' ' + clickNumber++;
   document.getElementsByTagName('body')[0].appendChild(newParagraph);
 });
+
+
+// Another method that worked
+// var heading = document.getElementById('h1');
+//
+// heading.addEventListener('click', function(event) {
+//   var newParagraph = document.createElement('p');
+//   newParagraph.innerHTML = 'This is click number' + ' ' + event.detail;
+//   document.getElementsByTagName('body')[0].appendChild(newParagraph);
+// });
