a54a54c572b87c0a2514159475bb4d250f725028
Brittany Bellanca <brittanybellanca@gmail.com>
2018-07-01 17:50:54 -0400

    finished setting up custom events & listeners

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
+// The first two are used together
 
-// listening for: mouse to enter; upon hearing: the background color will change
-var paragraph1 = document.getElementsByTagName('p')[0];
+var button = document. getElementsByClassName('button')[0];
+var article1 = document.getElementsByTagName('article')[0];
 
-paragraph1.addEventListener('mouseenter', function() {
-    paragraph1.className = 'yellow';
+// listening for: click on "button"; upon hearing: the background color will be removed
+button.addEventListener('click', function() {
+    article1.classList.remove('yellow')
 });
 
-paragraph1.addEventListener('mouseleave', function() {
-    paragraph1.classList.remove('yellow');
+// listening for: mouseover on article; upon hearing: the background color will appear again
+article1.addEventListener('mouseover', function() {
+    article1.className = 'main__article yellow'
 });
 
 
 // The next two are used together
-// listening for: click; upon hearing: will not go to website
+
 var pexelsLink = document.getElementsByTagName('a')[0];
 
+// listening for: click; upon hearing: will not go to website
 pexelsLink.addEventListener('click', function(event) {
     event.preventDefault();
 });
@@ -23,6 +27,9 @@ pexelsLink.addEventListener('click', function(event) {
 pexelsLink.addEventListener('dblclick', function() {
     var ouch = document.createElement('p');
     ouch.innerHTML = '<p class="ouch">Ouch! Oh, hi! Sorry, I guess I was sleeping on the job! Here, try this link <a class="text--white" href="https://www.pexels.com/photo/pink-daisy-flower-lot-1038002/" target="_blank">Pexels.com</a>!</p>';
-    var theSpot = document.getElementsByTagName('section')[1];
+    var theSpot = document.getElementsByTagName('article')[1];
     var theRightSpot = theSpot.getElementsByTagName('p')[0].appendChild(ouch);
 });
+
+
+// even though there are four Event Listeners, there are three that are unique, per requirements
