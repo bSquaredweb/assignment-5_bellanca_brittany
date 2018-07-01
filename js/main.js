5d42b8c91a84b4103d1396d140f3c95a78b4cee7
Brittany Bellanca <brittanybellanca@gmail.com>
2018-07-01 02:16:18 -0400

    began writing js for custom

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+// listening for: mouseover; upon hearing: the background color & text color will change
+var firstParagarph = document.getElementById('first');
+
+firstParagarph.addEventListener('mouseover', function() {
+
+});
+
+
+// listening for: double click; upon hearing: ___________
+var flowerImage = document.getElementsByTagName('img');
+
+flowerImage.addEventListener('dblclick', function() {
+
+});
+
+
+// listening for: _____; upon hearing: _____________
+var  = document.getElementById('id');
+
+ .addEventListener('keypress' function() {
+
+ });
